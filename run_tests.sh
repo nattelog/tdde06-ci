6124276d03c8343cb35730fa6a91e59f31777a50
Henke Adolfsson <catears13@gmail.com>
2017-02-28 16:32:59 +0100

    Updated run script

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-line="$(docker ps | grep henadpostgres)"
+line="$(docker ps | grep $POSTGRESNAME)"
 if [ -z "$line" ]
 then
-	echo "henadpostgres doesn't run, starting it"
-	docker run -p 5432:5432 --name henadpostgres -e POSTGRES_USER=postgres -e POSTGRES_DB=tdde06 -d postgres
+	echo "$POSTGRESNAME doesn't run, starting it"
+	docker run -p 5432:5432 --name $POSTGRESNAME -e POSTGRES_USER=postgres -e POSTGRES_DB=tdde06 -d postgres
 	sleep 10; # This works 142% of the time!
 fi
 
-docker run --link henadpostgres:postgres -P library/golang /bin/bash -c "git clone https://github.com/henziger/tdde06-ci.git && cd tdde06-ci && mkdir -p bin pkg src/main && python set_host.py && cp todo.go src/main/ && cp todo_test.go src/main/ && cd src/main/ && export GOPATH=/go/tdde06-ci && go get && cd ../../ && go test"
+docker run --link $POSTGRESNAME:postgres -P library/golang /bin/bash -c "git clone $GIT_URL tdde06 && cd tdde06 && mkdir -p bin pkg src/main && python set_host.py && cp todo.go src/main/ && cp todo_test.go src/main/ && cd src/main/ && export GOPATH=/go/tdde06 && go get && cd ../../ && go test"
 exit $?
