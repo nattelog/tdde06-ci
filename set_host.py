2e4cef5a41dade88bbc3e49fafb4547df603bd34
Henke Adolfsson <catears13@gmail.com>
2017-02-28 16:50:30 +0100

    does it work? I dont know

--- set_host.py
+++ set_host.py
@@ -2,6 +2,7 @@ import os
 
 hosts = open('/etc/hosts','r')
 hostname = os.environ['POSTGRESNAME']
+port = os.environ['POSTGRESPORT']
 for line in hosts:
     if hostname in line:
         host = line.split()[0]
@@ -12,6 +13,6 @@ with open('todo.go', 'r') as todo_file:
 
 # replace localhost with current host and write back to todo.go
 contents = contents.replace('databaseHost = "localhost"',
-                            'databaseHost = "%s"' % host)
+                            'databaseHost = "%s:%s"' % (host, port))
 with open('todo.go', 'w') as todo_file:
     todo_file.write(contents)
