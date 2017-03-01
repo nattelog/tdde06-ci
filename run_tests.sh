65b2e98efcb87a155fea6bc6812f73043b163626
Henke Adolfsson <catears13@gmail.com>
2017-03-01 13:36:06 +0100

    updated tests to not use port

--- run_tests.sh
+++ run_tests.sh
@@ -4,9 +4,9 @@ line="$(docker ps -a | grep $POSTGRESNAME)"
 if [ -z "$line" ]
 then
 	echo "$POSTGRESNAME doesn't run, starting it"
-	docker run -p $POSTGRESPORT:5432 --name $POSTGRESNAME -e POSTGRES_USER=postgres -e POSTGRES_DB=tdde06 -d postgres
+	docker run --name $POSTGRESNAME -e POSTGRES_USER=postgres -e POSTGRES_DB=tdde06 -d postgres
 	sleep 10; # This works 142% of the time!
 fi
 
-docker run --link $POSTGRESNAME:postgres -P library/golang /bin/bash -c "git clone $GIT_URL tdde06 && cd tdde06 && mkdir -p bin pkg src/main && export POSTGRESPORT=$POSTGRESPORT && export POSTGRESNAME=$POSTGRESNAME && python set_host.py && cp todo.go src/main/ && cp todo_test.go src/main/ && cd src/main/ && export GOPATH=/go/tdde06 && go get && cd ../../ && go test"
+docker run --link $POSTGRESNAME:postgres -P library/golang /bin/bash -c "git clone $GIT_URL tdde06 && cd tdde06 && mkdir -p bin pkg src/main && export POSTGRESNAME=$POSTGRESNAME && python set_host.py && cp todo.go src/main/ && cp todo_test.go src/main/ && cd src/main/ && export GOPATH=/go/tdde06 && go get && cd ../../ && go test"
 exit $?
